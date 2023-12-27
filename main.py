5e82a4bc072ad0b73ca0d4fddef9240cbbee0ffe
andriiprogs <andriiprogs@gmail.com>
2023-12-27 01:31:20 +0000

    f

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 x = 1
 y = 2
 z = -3.3
-
 a = x + y + z
+
 print("------------------------------------------ begin ------------")
 print(type(x))
 print(type(y))
 print(type(z))
-
-
 print(a)
 print(x)
 print(y)
