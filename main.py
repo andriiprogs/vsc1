b0a4b9aad145517b14a9eae16eb2aac6856315d3
andriiprogs <andriiprogs@gmail.com>
2023-12-27 01:30:36 +0000

    fix111

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ y = 2
 z = -3.3
 
 a = x + y + z
-
+print("------------------------------------------ begin ------------")
 print(type(x))
 print(type(y))
 print(type(z))
 
 
 print(a)
+print(x)
+print(y)
+print(z)
+print("------------------------------------------ end ----------------")
